d002f6b76fba21538c0da38c215fe0285715186c
Camerin <figueroa0609@protonmail.com>
2021-08-14 12:56:43 -0400

    Moved color options to it's own file

---
+++ src/options.js
@@ -0,0 +1,40 @@
+const options = [{
+    name: "Barkwood",
+    pickerImage: "https://www.gaf.com/-/media/shingleswatches/720x720swatches/timberline_hd-barkwood.jpg",
+    displayImages: ["https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/tlhdz-barkwood-house_1000.jpg", "https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/hdz-barkwood-orlando-house_1000.jpg", "https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/hdzbarkwoodorlandohouseaerial1000.jpg"],
+  },
+  {
+    name: "Mission Brown",
+    pickerImage: "https://www.gaf.com/-/media/shingleswatches/720x720swatches/timberline_hd-mission-brown.jpg",
+    displayImages: ["https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/tlhdz-missionbrown-house_1000.jpg", "https://www.serrano2roofing.com/wp-content/gallery/roof-replacement-and-chimney-repair-on-big-island-rd-in-warwick-ny/Drone-1.jpg", "https://www.serrano2roofing.com/wp-content/gallery/roof-replacement-and-chimney-repair-on-big-island-rd-in-warwick-ny/Drone-2.jpg", "https://www.serrano2roofing.com/wp-content/gallery/church-roof-replacement-in-scotchtown-ny/cover_photo.jpg"]
+  },
+  {
+    name: "Pewter Gray",
+    pickerImage: "https://www.gaf.com/-/media/shingleswatches/720x720swatches/timberline_hd-pewter_gray.jpg",
+    displayImages: ["https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/tlhdz-pewtergray-house_1000.jpg", "https://www.serrano2roofing.com/wp-content/gallery/roof-replacement-with-gaf-hdz-in-pewter-gray/roof-replacement-gaf-hdz-pewter-gray-7__after.jpg"]
+  },
+  {
+    name: "Birchwood",
+    pickerImage: "https://www.gaf.com/-/media/shingleswatches/720x720swatches/timberline_hd-birchwood.jpg",
+    displayImages: ["https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/tlhdz-birchwood-house_1000.jpg"],
+  },
+  {
+    name: "Charcoal",
+    pickerImage: "https://www.gaf.com/-/media/shingleswatches/720x720swatches/timberline_hd-charcoal.jpg",
+    displayImages: ["https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/tlhdz-charcoal-house_1000.jpg", "https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/hdz-charcoal-windproven-brickhouse-aerialshot_1000.jpg", ],
+  },
+  {
+    name: "Biscayne Blue",
+    pickerImage: "https://www.gaf.com/-/media/shingleswatches/720x720swatches/timberline_hd-biscayne_blue.jpg",
+    displayImages: ["https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/tlhdz-biscayneblue-house_1000.jpg"],
+  },
+  {
+    name: "Driftwood",
+    pickerImage: "https://www.gaf.com/-/media/shingleswatches/720x720swatches/timberline_hd-driftwood.jpg",
+    displayImages: ["https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/tlhdz-driftwood-house_1000.jpg"],
+  },
+  
+
+];
+
+export default options;
