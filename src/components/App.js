9ef1f16942e384c06dd6ee09761fc99a203ee5e9
Camerin <figueroa0609@protonmail.com>
2021-08-14 10:55:43 -0400

    Moved bootstrap css and added own css

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React, {useState} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Row, Col} from 'react-bootstrap';
 import Picker from './Picker';
 import Display from './Display';
 
 const options = [
   { name: "Barkwood", pickerImage: "https://www.gaf.com/-/media/shingleswatches/720x720swatches/timberline_hd-barkwood.jpg", displayImages: ["https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/tlhdz-barkwood-house_1000.jpg", "https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/hdz-barkwood-orlando-house_1000.jpg", "https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/hdzbarkwoodorlandohouseaerial1000.jpg"],},
+  { name: "Mission Brown", pickerImage: "https://www.gaf.com/-/media/shingleswatches/720x720swatches/timberline_hd-mission-brown.jpg", displayImages: ["https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/tlhdz-missionbrown-house_1000.jpg", "https://www.serrano2roofing.com/wp-content/gallery/roof-replacement-and-chimney-repair-on-big-island-rd-in-warwick-ny/Drone-1.jpg", "https://www.serrano2roofing.com/wp-content/gallery/roof-replacement-and-chimney-repair-on-big-island-rd-in-warwick-ny/Drone-2.jpg", "https://www.serrano2roofing.com/wp-content/gallery/church-roof-replacement-in-scotchtown-ny/cover_photo.jpg"]},
   { name: "Birchwood", pickerImage: "https://www.gaf.com/-/media/shingleswatches/720x720swatches/timberline_hd-birchwood.jpg", displayImages: ["https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/tlhdz-birchwood-house_1000.jpg"],},
   { name: "Charcoal", pickerImage: "https://www.gaf.com/-/media/shingleswatches/720x720swatches/timberline_hd-charcoal.jpg", displayImages: ["https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/tlhdz-charcoal-house_1000.jpg", "https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/hdz-charcoal-windproven-brickhouse-aerialshot_1000.jpg", ],},
   { name: "Biscayne Blue", pickerImage: "https://www.gaf.com/-/media/shingleswatches/720x720swatches/timberline_hd-biscayne_blue.jpg", displayImages: ["https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/tlhdz-biscayneblue-house_1000.jpg"],},
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './components/App.css';
+
 
 ReactDOM.render(
     <App />,
