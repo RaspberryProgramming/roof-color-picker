cce70ff14facff769208c396d1535e7aa7f0d4e7
Camerin <figueroa0609@protonmail.com>
2021-08-15 17:18:22 -0400

    added active tags to options

--- src/options.js
+++ src/options.js
@@ -1,37 +1,45 @@
-const options = [{
+const options = [
+  {
     name: "Barkwood",
     pickerImage: "https://www.gaf.com/-/media/shingleswatches/720x720swatches/timberline_hd-barkwood.jpg",
     displayImages: ["https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/tlhdz-barkwood-house_1000.jpg", "https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/hdz-barkwood-orlando-house_1000.jpg", "https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/hdzbarkwoodorlandohouseaerial1000.jpg"],
+    active: true,
   },
   {
     name: "Mission Brown",
     pickerImage: "https://www.gaf.com/-/media/shingleswatches/720x720swatches/timberline_hd-mission-brown.jpg",
-    displayImages: ["https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/tlhdz-missionbrown-house_1000.jpg", "https://www.serrano2roofing.com/wp-content/gallery/roof-replacement-and-chimney-repair-on-big-island-rd-in-warwick-ny/Drone-1.jpg", "https://www.serrano2roofing.com/wp-content/gallery/roof-replacement-and-chimney-repair-on-big-island-rd-in-warwick-ny/Drone-2.jpg", "https://www.serrano2roofing.com/wp-content/gallery/church-roof-replacement-in-scotchtown-ny/cover_photo.jpg"]
+    displayImages: ["https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/tlhdz-missionbrown-house_1000.jpg", "https://www.serrano2roofing.com/wp-content/gallery/roof-replacement-and-chimney-repair-on-big-island-rd-in-warwick-ny/Drone-1.jpg", "https://www.serrano2roofing.com/wp-content/gallery/roof-replacement-and-chimney-repair-on-big-island-rd-in-warwick-ny/Drone-2.jpg", "https://www.serrano2roofing.com/wp-content/gallery/church-roof-replacement-in-scotchtown-ny/cover_photo.jpg"],
+    active: true,
   },
   {
     name: "Pewter Gray",
     pickerImage: "https://www.gaf.com/-/media/shingleswatches/720x720swatches/timberline_hd-pewter_gray.jpg",
-    displayImages: ["https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/tlhdz-pewtergray-house_1000.jpg", "https://www.serrano2roofing.com/wp-content/gallery/roof-replacement-with-gaf-hdz-in-pewter-gray/roof-replacement-gaf-hdz-pewter-gray-7__after.jpg"]
+    displayImages: ["https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/tlhdz-pewtergray-house_1000.jpg", "https://www.serrano2roofing.com/wp-content/gallery/roof-replacement-with-gaf-hdz-in-pewter-gray/roof-replacement-gaf-hdz-pewter-gray-7__after.jpg"],
+    active: true,
   },
   {
     name: "Hunter Green",
     pickerImage: "https://www.gaf.com/-/media/shingleswatches/720x720swatches/timberline_hd-hunter_green.jpg",
     displayImages: ["https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/tlhdz-huntergreen-house_1000.jpg", "https://www.serrano2roofing.com/wp-content/gallery/hunter-green-roof-replacement-in-wurtsboro/hunter-green-roof-replacement-in-wurtsboro-7.png", "https://www.serrano2roofing.com/wp-content/gallery/hunter-green-roof-replacement-in-wurtsboro/hunter-green-roof-replacement-in-wurtsboro-6.jpg"],
+    active: true,
   },
   {
     name: "Charcoal",
     pickerImage: "https://www.gaf.com/-/media/shingleswatches/720x720swatches/timberline_hd-charcoal.jpg",
     displayImages: ["https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/tlhdz-charcoal-house_1000.jpg", "https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/hdz-charcoal-windproven-brickhouse-aerialshot_1000.jpg", ],
+    active: true,
   },
   {
     name: "Birchwood",
     pickerImage: "https://www.gaf.com/-/media/shingleswatches/720x720swatches/timberline_hd-birchwood.jpg",
     displayImages: ["https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/tlhdz-birchwood-house_1000.jpg"],
+    active: true,
   },
   {
     name: "Biscayne Blue",
     pickerImage: "https://www.gaf.com/-/media/shingleswatches/720x720swatches/timberline_hd-biscayne_blue.jpg",
     displayImages: ["https://www.gaf.com/-/media/shingleinstalledhomes/1000x1000/hdz/tlhdz-biscayneblue-house_1000.jpg"],
+    active: true,
   },
 ];
 
