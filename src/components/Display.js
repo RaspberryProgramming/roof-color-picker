044cc3033b62ca0677c43edb9f4f979e48e6d2aa
Camerin <figueroa0609@protonmail.com>
2021-08-15 16:49:12 -0400

    Fixed Carousel indicators and added a open button

--- src/components/Display.js
+++ src/components/Display.js
@@ -6,6 +6,9 @@ const Display = ({options, selected, colorSelected, newColorSelected }) => {
     return (
       <Carousel.Item key={index} className={`${index === 0 && colorSelected ? 'active': ''}`}>
         <img alt={index} src={image} className="d-block w-100"/>
+          <Carousel.Caption>
+            <a className="btn btn-dark" href={image} target="_blank" rel="noreferrer">Open in Another Tab</a>
+          </Carousel.Caption>
       </Carousel.Item>
     );
   });
@@ -16,7 +19,7 @@ const Display = ({options, selected, colorSelected, newColorSelected }) => {
 
   return (
     <div className="display">
-      <Carousel>
+      <Carousel nextLabel={""} prevLabel={""}>
         {images}
       </Carousel>
     </div>
